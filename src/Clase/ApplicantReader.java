bc56d0690cbb858b46ed7079259a74a0a28c4df0
Notinaru Ana <ana_maria_not24@yahoo.com>
2022-04-05 13:19:32 +0300

    Commit seminar 3

--- src/Clase/ApplicantReader.java
+++ src/Clase/ApplicantReader.java
@@ -1,24 +1,8 @@
 package Clase;
 
-import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.util.List;
 
-public class ApplicantReader {
-    public static void citireAplicant(Scanner scanner, Aplicant aplicant){
-        String nume = scanner.next();
-        String prenume = scanner.next();
-        int varsta = scanner.nextInt();
-        int punctaj = scanner.nextInt();
-        int nr = scanner.nextInt();
-        String[] proiecte = new String[nr];
-        for (int i = 0; i < nr; i++)
-            proiecte[i] = scanner.next();
-        aplicant.setNume(nume);
-        aplicant.setNume(prenume);
-        aplicant.setVarsta(varsta);
-        aplicant.setPunctaj(punctaj);
-        aplicant.setDenumiriProiecte(nr, proiecte);
-        for (int i=0;i<nr;i++) {
-            aplicant.denumireProiect[i] = proiecte[i];
-        }
-    }
+public interface ApplicantReader {
+    List<Aplicant> citireAplicanti(String numeFisier) throws FileNotFoundException;
 }
